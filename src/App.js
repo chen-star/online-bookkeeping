cf73bf4a89ab7a7a70e693344d935178cffac780
jxchen <jxchen31@gmail.com>
2019-05-11 13:18:38 -0400

    [impl] finished static price list and price item

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PriceList from './components/PriceList'
+
+const items = [
+    {
+        "id": 1,
+        "title": "去云南旅游",
+        "price": 2000,
+        "date": "2019-03-05",
+        "category": {
+            "id": 1,
+            "name": "旅行",
+            "type": "outcome"
+        }
+    },
+    {
+        "id": 2,
+        "title": "吃全聚德",
+        "price": 160,
+        "date": "2019-04-03",
+        "category": {
+            "id": 1,
+            "name": "餐饮",
+            "type": "outcome"
+        }
+    }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+            <PriceList items={items}
+                       onModifyItem={(item) => {
+                           alert(item.id)
+                       }}
+                       onDeleteItem={(item) => {
+                           alert(item.id)
+                       }}
+            />
+        </div>
+    );
 }
 
 export default App;
